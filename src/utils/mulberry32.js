15186dcc853534832312e936b6bc3c667a3b5767
Andres6936 <joan.buritica@artemisa.tech>
2023-01-09 22:57:09 -0500

    Added Documentation

--- src/utils/mulberry32.js
+++ src/utils/mulberry32.js
@@ -34,6 +34,49 @@ export default function mulberry32 (seed) {
   }
 }
 
+/**
+ * Algorithm based MurmurHash3's mixing function.
+ *
+ * Most of the generators here have no built-in seed generating procedure
+ * (for sake of simplicity), but accept one or more 32-bit values as the
+ * initial state of the PRNG. Similar seeds (e.g. a simple seed of 1 and 2)
+ * can cause correlations in weaker PRNGs, resulting in the output having
+ * similar properties (such as randomly generated levels being similar).
+ * To avoid this, it is best practice to initialize PRNGs with a
+ * well-distributed seed.
+ *
+ * There are various ways to seed a PRNG, but care must be taken. Certain
+ * generators have their own seeding procedure (jsf32, sfc32, gjrand32,
+ * v3b), which typically fill the state with some preset data and run the
+ * generator a few times in advance. This appears to work, and is often
+ * by design, but it can reduce the effective number of input states
+ * dramatically.
+ *
+ * I propose using a separate hash function to initialize the entire
+ * state. Hash functions are very good at generating seeds for PRNGs
+ * from short strings. A good hash function will generate very different
+ * results even when two strings are similar.
+ *
+ * Each subsequent call to the return function of xmur3 produces a new
+ * "random" 32-bit hash value to be used as a seed in a PRNG. Here's
+ * how you might use it:
+ *
+ * @example
+ * // Create xmur3 state:
+ * var seed = xmur3("apples");
+ *
+ * // Output one 32-bit hash to provide the seed for mulberry32.
+ * var rand = mulberry32(seed());
+ *
+ * // Obtain sequential random numbers like so:
+ * rand();
+ *
+ *
+ * @param {string} str The seed to use. This param cannot be null|undefined
+ * @return {function(): number} The generator random seed function. Each
+ * subsequent call to the return function of xmur3 produces a new "random"
+ * 32-bit hash value to be used as a seed in a PRNG.
+ */
 function xmur3 (str) {
   for (var i = 0, h = 1779033703 ^ str.length; i < str.length; i++) {
     h = Math.imul(h ^ str.charCodeAt(i), 3432918353)
