35e853a8d51822ba40f4e8f029d8d98f65050a04
Andres6936 <andres6936@gmail.com>
2023-05-30 11:43:55 -0500

    Added React Project

--- showcase/src/App.tsx
+++ showcase/src/App.tsx
@@ -1,4 +1,31 @@
+import trianglify from 'adan-trianglify'
+import {useEffect} from "react";
+
 export default function App() {
+    useEffect(() => {
+        const pattern = trianglify({
+    cellSize: 50,
+    width: window.innerWidth * 0.8,
+    height: (window.innerHeight - 4 * 30) / 4
+        })
+
+        // Render to SVG
+        document.body.appendChild(pattern.toSVG())
+        // Render to Canvas
+        document.body.appendChild(pattern.toCanvas())
+
+        // stroke-only
+        const strokeOnly = trianglify({
+            cellSize: 75,
+            width: window.innerWidth * 0.8,
+            height: (window.innerHeight - 4 * 30) / 4,
+            fill: false,
+            strokeWidth: 1
+        })
+        document.body.appendChild(strokeOnly.toSVG())
+        document.body.appendChild(strokeOnly.toCanvas())
+    }, [])
+
     return (
         <div>
             
