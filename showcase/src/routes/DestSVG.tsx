2a67d88ee8decfc0d5fa6a110840e25202da32f3
Andres6936 <andres6936@gmail.com>
2023-05-30 18:28:43 -0500

    Added New Path

--- showcase/src/routes/DestSVG.tsx
+++ showcase/src/routes/DestSVG.tsx
@@ -1,5 +1,22 @@
+import {useEffect} from "react";
+import trianglify from "adan-trianglify";
+
 export default function DestSVG(){
+    useEffect(() => {
+
+        const pattern = trianglify({
+            cellSize: 50,
+            width: window.innerWidth * 0.8,
+            height: (window.innerHeight - 4 * 30) / 4
+        })
+
+        const existingSVGElement = document.getElementById('svg-element')
+
+        // Render to SVG
+        pattern.toSVG(existingSVGElement)
+    }, [])
+
     return (
-        <div></div>
+        <svg id="svg-element"></svg>
     )
 }
