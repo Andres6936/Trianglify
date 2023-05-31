323b21c8fc575a737057a4213c7d8b677efe22a2
Andres6936 <andres6936@gmail.com>
2023-05-30 19:00:50 -0500

    Added Master CSS

--- showcase/src/main.tsx
+++ showcase/src/main.tsx
@@ -1,11 +1,13 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import {createBrowserRouter, RouterProvider} from "react-router-dom";
+import { CSSProvider } from '@master/css.react'
 import App from './routes/App.tsx'
 import ColorFunction from "./routes/ColorFunction.tsx";
 import CustomPoints from "./routes/CustomPoints.tsx";
 import DestSVG from "./routes/DestSVG.tsx";
 import Transparency from "./routes/Transparency.tsx";
+import config from "../master.css.ts";
 
 const router = createBrowserRouter([
     {
@@ -34,6 +36,8 @@ const router = createBrowserRouter([
 
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
     <React.StrictMode>
-        <RouterProvider router={router}/>
+        <CSSProvider config={config}>
+            <RouterProvider router={router}/>
+        </CSSProvider>
     </React.StrictMode>,
 )
