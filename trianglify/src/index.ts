0e41d14acea0504c241af3761456265726815327
Andres6936 <andres6936@gmail.com>
2023-05-31 14:44:14 -0500

    Moved Functions

--- trianglify/src/index.ts
+++ trianglify/src/index.ts
@@ -10,14 +10,14 @@ import Delaunator from 'delaunator'
 // (chroma bloats bundle by 40k, minified)
 import chroma, {Scale} from 'chroma-js'
 
-import colorbrewer from './utils/colorbrewer'
-import Pattern from './utils/pattern'
-import mulberry32 from './utils/mulberry32'
-import * as geom from './utils/geom'
-import * as colorFunctions from './utils/colorFunctions'
-import type {Options} from "./types/Options";
 import type {BrewerPalettes} from "./types/BrewerPalettes";
+import type {Options} from "./types/Options";
 import type {Point} from "./types/Point";
+import colorbrewer from './color/ColorBrewer'
+import Pattern from './utils/Pattern'
+import mulberry32 from './random/Mulberry32'
+import * as geom from './utils/Geometry'
+import * as colorFunctions from './color/ColorFunctions'
 
 const defaultOptions: Options = {
   width: 600,
